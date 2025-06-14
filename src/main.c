900fefd297d12e2e159a24b9e12d248b2b439048
Abigail Adegbiji <adegbijiabigail@gmail.com>
2025-06-14 16:00:33 -0600

    basic mvp

--- src/main.c
+++ src/main.c
@@ -12,31 +12,24 @@ typedef enum {
     PieceEnumSize
 } Piece;
 
-Piece getPiece(char c) {
-    if (c == '#') return Wall;
-    if (c == '@' || c == 'p') return Pusher;
-    if (c == 'b' || c == '$') return Box;
-    if (c == 'B' || c == '*') return Goal;
-    return Floor;
-}
-
 typedef struct {
     Vector2 size;
-    Piece* pieces;
-    bool loaded;
-} Board;
+    int numBytes;
+    Piece* tiles;
+    Piece* original;
+} Level;
 
-Board* loadBoards(const char* textFile, int amount) {
+Level* loadLevels(const char* textFile, int amount) {
     char* source = LoadFileText(textFile);
 
     char* prev = NULL;
     char* current = source;
-    char* boardStart = source;
+    char* levelStart = source;
 
     Vector3 pos = { 0, 0 };
-    bool foundBoardSize = false;
+    bool foundLevelSize = false;
 
-    Board* boards = calloc(amount, sizeof(Board));
+    Level* levels = calloc(amount, sizeof(Level));
     int i = 0;
 
     while (*current != '\0') {
@@ -48,57 +41,63 @@ Board* loadBoards(const char* textFile, int amount) {
 	    // Each puzzle is seperated by an empty line
 	    bool newline = *prev == *current;
  
-	    if (newline && !foundBoardSize) {
+	    if (newline && !foundLevelSize) {
 		// backtrace, since
-		// we do a first pass to get the size of the boards
-		// then another pass to get the contents of the board
-		current = boardStart;
-		boards[i].size.y = pos.y;
+		// we do a first pass to get the size of the levels
+		// then another pass to get the contents of the level
+		current = levelStart;
+		levels[i].size.y = pos.y;
 		pos.x = pos.y = 0;
-		foundBoardSize = true;
+		foundLevelSize = true;
 
-		int total = boards[i].size.x * boards[i].size.y;
-		boards[i].pieces = calloc(total, sizeof(Piece));
+		levels[i].numBytes = levels[i].size.x * levels[i].size.y * sizeof(Piece);
+		levels[i].tiles = malloc(levels[i].numBytes);
+		levels[i].original = malloc(levels[i].numBytes);
+		memset(levels[i].tiles, Floor, levels[i].numBytes);
 		continue;
-	    } else if (newline && foundBoardSize) {
-		// Read the board size and the board contents, move on to the next one
+	    } else if (newline && foundLevelSize) {
+		// Read the level size and the level contents, move on to the next one
 		if (++i >= amount) break;
-		boards[i].loaded = true;
 		pos.x = pos.y = 0;
-		foundBoardSize = false;
-		boardStart = current;
+		foundLevelSize = false;
+		levelStart = current;
 		continue;
 	    }
 
 	    // new row
-	    boards[i].size.x = fmax(boards[i].size.x, pos.x + 1); // linex can be different lengths
+	    levels[i].size.x = fmax(levels[i].size.x, pos.x + 1); // linex can be different lengths
 	    pos.x = 0;
 	    pos.y++;
 	    continue;
 	}
 
-	// Read the board contents
+	// Read the level contents
 	pos.x++;
-	if (foundBoardSize) {
-	    int index = pos.y * boards[i].size.x + pos.x;
-	    boards[i].pieces[index] = getPiece(*current);
+	if (foundLevelSize) {
+	    int index = pos.y * levels[i].size.x + pos.x;
+	    Piece p = Floor;
+	    if (*current == '#') p = Wall;
+	    if (*current == '@') p = Pusher;
+	    if (*current == '$') p = Box;
+	    if (*current == '*') p = Goal;
+	    levels[i].tiles[index] = p;
 	}
     }
 
     UnloadFileText(source);
-    return boards;
+    return levels;
 }
 
-void cleanupBoards(Board* boards, int amount) {
+void cleanupLevels(Level* levels, int amount) {
     for (int i = 0; i < amount; i++) {
-	free(boards[i].pieces);
+	free(levels[i].tiles);
+	free(levels[i].original);
     }
-    free(boards);
+    free(levels);
 }
 
 typedef struct {
     Model model;
-    Vector3 size;
     Vector3 scaleFactor;
 } Asset;
 
@@ -107,10 +106,10 @@ Asset loadAsset(const char* path, Vector3 targetSize) {
     asset.model = LoadModel(path);
 
     BoundingBox bounds = GetMeshBoundingBox(asset.model.meshes[0]);
-    asset.size = Vector3Subtract(bounds.max, bounds.min);
+    Vector3 size = Vector3Subtract(bounds.max, bounds.min);
 
     // scale with correct aspect ratio
-    float maxDim = fmax(fmax(asset.size.x, asset.size.y), asset.size.z); // biggest side
+    float maxDim = fmax(fmax(size.x, size.y), size.z); // biggest side
     Vector3 f = { targetSize.x / maxDim , targetSize.y / maxDim, targetSize.z / maxDim};
     asset.scaleFactor = targetSize.x > 0 ? f : Vector3One();
     return asset;
@@ -123,25 +122,40 @@ typedef struct {
 
     Vector2 player;
 
-    Board* boards;
-    int numBoards;
+    Level* levels;
+    int goalPositions[14];
+    int numLevels;
     int level;
+    bool levelSolved;
 } Game;
 
 void changeLevel(Game* game, bool next) {
     game->level = next ? game->level + 1 : game->level - 1;
-    game->level = Clamp(game->level, 0, game->numBoards);
+    game->level = Clamp(game->level, 0, game->numLevels - 1);
+    Level level = game->levels[game->level];
 
-    // find the player
-    Board board = game->boards[game->level];
-    for (int y = 0; y < board.size.y; y++) {
-	for (int x = 0; x < board.size.x; x++) {
-	    int index = y * board.size.x + x;
-	    if (board.pieces[index] == Pusher) {
-		board.pieces[index] = Floor;
+    int i = 0;
+    int len = sizeof(game->goalPositions) / sizeof(int);
+    memset(game->goalPositions, -1, sizeof(game->goalPositions));
+
+    // Find the player and store each goal position
+
+    for (int y = 0; y < level.size.y; y++) {
+	for (int x = 0; x < level.size.x; x++) {
+	    int index = y * level.size.x + x;
+	    // find the player
+	    if (level.original[index] == Pusher) {
+		level.tiles[index] = Floor;
 		game->player.x = x;
 		game->player.y = y;
-		break;
+	    }
+
+	    if (level.original[index] == Goal) {
+		if (i >= len) {
+		    printf("no more space!\n");
+		    break;
+		}
+		game->goalPositions[i++] = index;	
 	    }
 	}
     }
@@ -150,12 +164,13 @@ void changeLevel(Game* game, bool next) {
 Game newGame() {
     Game game;
 
+    // Load the assets
     game.tileSize = (Vector3){ 2, 2, 2 }; 
     game.assets[Box] = loadAsset("../assets/box_small.gltf", game.tileSize);
     game.assets[Wall] = loadAsset("../assets/wall.gltf", game.tileSize);
     game.assets[SplitWall] = loadAsset("../assets/wall_Tsplit.gltf", game.tileSize);
     game.assets[Corner] = loadAsset("../assets/wall_corner.gltf", game.tileSize);
-    game.assets[Floor] = loadAsset("../assets/floor_wood_small.gltf", game.tileSize);
+    game.assets[Floor] = loadAsset("../assets/floor_wood_small_dark.gltf", game.tileSize);
     game.assets[Goal] = loadAsset("../assets/floor_dirt_small_A.gltf", game.tileSize);
     game.assets[Pusher] = loadAsset("../assets/banner_red.gltf", (Vector3){0, 0, 0});
     game.texture = LoadTexture("../assets/dungeon_texture.png");
@@ -164,13 +179,42 @@ Game newGame() {
 	m.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = game.texture;
     }
 
-    game.numBoards = 27;
-    game.boards = loadBoards("../src/levels.txt", game.numBoards);
+    // Load each level and keep an original copy of them
+    game.numLevels = 27;
+    game.levels = loadLevels("../src/levels.txt", game.numLevels);
+    for (int i = 0; i < game.numLevels; i++) {
+	memcpy(game.levels[i].original, game.levels[i].tiles, game.levels[i].numBytes);
+    }
 
     game.level = -1;
+    game.levelSolved = false;
     changeLevel(&game, true);
 
     return game;
+}
+
+void checkProgress(Game* game) {
+    game->levelSolved = true;
+    int len = sizeof(game->goalPositions) / sizeof(int);
+
+    for (int i = 0; i < len; i++) {
+	int pos = game->goalPositions[i];
+	if (pos == -1) break; // no more goal positions
+	if (game->levels[game->level].tiles[pos] != Box) {
+	    game->levelSolved = false;
+	    break;
+	}
+    }
+}
+
+void restartGame(Game* game) {
+    memcpy(
+	game->levels[game->level].tiles,
+	game->levels[game->level].original,
+	game->levels[game->level].numBytes
+    );
+    game->level--;
+    changeLevel(game, true);
 }
 
 void cleanupGame(Game* game) {
@@ -179,7 +223,7 @@ void cleanupGame(Game* game) {
     }
     UnloadTexture(game->texture);
 
-    cleanupBoards(game->boards, game->numBoards);
+    cleanupLevels(game->levels, game->numLevels);
 }
 
 typedef struct {
@@ -188,12 +232,12 @@ typedef struct {
     float rotation;
 } Border;
 
-Border computeBorder(Board board, int x, int y) {
-    int w = board.size.x, h = board.size.y;
-    bool l = x - 1 < 0  || board.pieces[y * w + (x - 1)] != Wall; // left empty?
-    bool r = x + 1 >= w || board.pieces[y * w + (x + 1)] != Wall; // right empty?
-    bool t = y - 1 < 0  || board.pieces[(y - 1) * w + x] != Wall; // top empty?
-    bool b = y + 1 >= h || board.pieces[(y + 1) * w + x] != Wall; // bottom empty?
+Border computeBorder(Level level, int x, int y) {
+    int w = level.size.x, h = level.size.y;
+    bool l = x - 1 < 0  || level.tiles[y * w + (x - 1)] != Wall; // left empty?
+    bool r = x + 1 >= w || level.tiles[y * w + (x + 1)] != Wall; // right empty?
+    bool t = y - 1 < 0  || level.tiles[(y - 1) * w + x] != Wall; // top empty?
+    bool b = y + 1 >= h || level.tiles[(y + 1) * w + x] != Wall; // bottom empty?
 
     // corners
     if (!l && r && t && !b) return (Border){ false, true, 0 };   // top right
@@ -211,12 +255,12 @@ Border computeBorder(Board board, int x, int y) {
     return l && r ? (Border){ false, false, 90 } : (Border){ false, false, 0 };
 }
 
-void getFirstAndLastWalls(Board b, int row, int* indexFirst, int* indexLast) {
+void getFirstAndLastWalls(Level b, int row, int* indexFirst, int* indexLast) {
     *indexFirst = b.size.x;
     *indexLast = 0;
     for (int i = 0; i < b.size.x; i++) {
 	int index = row * b.size.x + i;
-	if (b.pieces[index] == Wall) {
+	if (b.tiles[index] == Wall) {
 	    if (i < *indexFirst) *indexFirst = i;
 	    if (i > *indexLast) *indexLast = i;
 	}
@@ -224,22 +268,22 @@ void getFirstAndLastWalls(Board b, int row, int* indexFirst, int* indexLast) {
 }
 
 void drawLevel(Game* game) {
-    Board board = game->boards[game->level];
+    Level level = game->levels[game->level];
 
     Vector2 start = {
-	-(floor(board.size.x / 2)) * game->tileSize.x,
-	-(floor(board.size.y / 2)) * game->tileSize.y,
+	-(floor(level.size.x / 2)) * game->tileSize.x,
+	-(floor(level.size.y / 2)) * game->tileSize.y,
     };
     Vector3 pos = { start.x, 0, start.y };
 
-    for (int i = 0; i < board.size.y; i++) {
+    for (int i = 0; i < level.size.y; i++) {
 	int first, last;
-	getFirstAndLastWalls(board, i, &first, &last);
+	getFirstAndLastWalls(level, i, &first, &last);
 
-	for (int j = 0; j < board.size.x; j++) {
+	for (int j = 0; j < level.size.x; j++) {
 	    float angle = 0;
 	    Vector3 axis = {0, 1, 0};
-	    Piece p = board.pieces[i * (int)board.size.x + j];
+	    Piece p = level.tiles[i * (int)level.size.x + j];
 	    Asset a = game->assets[p];
 
 	    bool player = j == game->player.x && i == game->player.y;
@@ -247,7 +291,7 @@ void drawLevel(Game* game) {
 
 	    // Figure out how to draw the wall
 	    if (p == Wall) {
-		Border b = computeBorder(board, j, i);
+		Border b = computeBorder(level, j, i);
 		if (b.corner) a = game->assets[Corner];
 		if (b.splitWall) a = game->assets[SplitWall];
 		angle = b.rotation;
@@ -270,32 +314,58 @@ void drawLevel(Game* game) {
     }
 }
 
-/*
-__xx_x<
-___x<
+void pushBoxes(Level b, Vector2 next, bool* canMove, int x, int y) {
+    // The player can only push a chain of boxes horizontally or vertically
+    // We need to figure out where the last box they'll be pushing is
+    Vector2 end = next;
+    while (true) {
+	Piece p = b.tiles[(int)(end.y * b.size.x + end.x)];
+	if (p != Box) {
+	    *canMove = p != Wall;
+	    break;
+	}
+	end.x += x;
+	end.y += y;
+    }
 
-_xx_
-_xx<
-
-can only push on one line
-horizontal, vertical
-*/
-void pushBoxes(Game* game, Vector2 nextPosition) {
+    // We can only push one box at a time, so we can just
+    // move the box on the tile the player will move to next
+    // to the end of the chain
+    if (*canMove) {
+	int endIndex = end.y * b.size.x + end.x;
+	int nextIndex = next.y * b.size.x + next.x;
+	b.tiles[endIndex] = b.tiles[nextIndex];
+	// preserve goal tiles:
+	Piece p = b.original[nextIndex] != Box ? b.original[nextIndex] : Floor;
+	b.tiles[nextIndex] = p;
+    }
 }
 
 void movePlayer(Game* game, int deltaX, int deltaY) {
-    Board b = game->boards[game->level];
-    Vector2 next = {
-	fmax(0, fmin(game->player.x + deltaX, b.size.x)),
-	fmax(0, fmin(game->player.y + deltaY, b.size.y))
-    };
+    Level b = game->levels[game->level];
+    Vector2 next = { game->player.x + deltaX, game->player.y + deltaY };
 
     int index = next.y * b.size.x + next.x;
-    if (b.pieces[index] == Wall) return;
+    if (b.tiles[index] == Wall) return;
 
-    if (b.pieces[index] == Box) pushBoxes(game, next);
+    if (b.tiles[index] == Box) {
+	bool canPushBoxes = false;
+	pushBoxes(b, next, &canPushBoxes, deltaX, deltaY);
+	if (!canPushBoxes) return;
+    }
+
     game->player = next;
+    checkProgress(game);
 }
+
+// TODO: get better 3d models
+//	 extract levels again -- didn't get them properly the first time
+// 	 add menu transition between levels
+// 	 add basic lighting
+// 	 add a basic titlescreen
+// 	 add ui buttons
+// 	 port to web and mobile
+// 	 release
 
 int main() {
     SetTraceLogLevel(LOG_WARNING);
@@ -314,6 +384,7 @@ int main() {
     while (!WindowShouldClose()) {
 	if (IsKeyPressed(KEY_L)) changeLevel(&game, true);
 	if (IsKeyPressed(KEY_H)) changeLevel(&game, false);
+	if (IsKeyPressed(KEY_R)) restartGame(&game);
 	if (IsKeyPressed(KEY_RIGHT)) movePlayer(&game, 1, 0);
 	if (IsKeyPressed(KEY_LEFT)) movePlayer(&game, -1, 0);
 	if (IsKeyPressed(KEY_UP)) movePlayer(&game, 0, -1);
@@ -323,7 +394,9 @@ int main() {
 
 	BeginDrawing();
 	ClearBackground(RAYWHITE);
-	DrawText(TextFormat("%d", game.level), 20, 10, 20, BLACK);
+	DrawText(TextFormat("%d", game.level + 1), 20, 10, 20, BLACK);
+	if (game.levelSolved)
+	    DrawText("level solved!", 20, 10, 20, BLACK);
 
 	BeginMode3D(camera);
 	drawLevel(&game);
